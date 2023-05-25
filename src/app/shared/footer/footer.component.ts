1d2ff2fa0d1f90c67aae941bfdd2f476acdfbcc7
Josias-Ribeiro <josias_ribeiro90@hotmail.com>
2023-05-25 07:32:40 -0300

    Criacao componentes header e footer

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './view/footer.component.html',
+  styleUrls: ['./view/footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './view/header.component.html',
+  styleUrls: ['./view/header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
